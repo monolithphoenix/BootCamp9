e20b3119ee9b6ec3309291184229b32e47ad948c
iRoy <iroy.photo@gmail.com>
2019-01-17 18:51:41 +0200

    JS CW 20190117

---
+++ JS/CW/class_work-20190117/js/index.js
@@ -0,0 +1,14 @@
+// arrow function
+    function add (a, b) {
+        return a + b;
+    };
+
+    const add = (a, b) => {
+        return a + b;
+    };
+
+    const add = (a, b) => a + b;
+
+    const add = str => str + "!";
+
+
