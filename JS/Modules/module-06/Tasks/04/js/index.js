7540d7fd93391aeee14a883cb9d9c91a6d089f64
iRoy <iroy.photo@gmail.com>
2019-01-24 17:37:32 +0200

    Task 04 NOT!!! completed

--- JS/Modules/module-06/Tasks/04/js/index.js
+++ JS/Modules/module-06/Tasks/04/js/index.js
@@ -12,3 +12,51 @@
   
   Car.getSpecs(someCar); // maxSpeed: 100, running: true, distance: 200
 */
+class Car {
+  constructor(maxSpeed) {
+    this.speed = 0;
+    this.maxSpeed = maxSpeed;
+    this.running = false;
+    this.distance = 0;
+  };
+
+  getSpecs(arg) {
+    console.log(arg.maxSpeed);
+  };
+
+  turnOn() {
+    this.running = true;
+  };
+
+  turnOff() {
+    this.running = false;
+  };
+  
+  accelerate(spd) {
+    if (spd < this.maxSpeed) {
+      this.speed = spd;
+    };
+  };
+  
+  decelerate(spd) {
+    if (0 < spd < this.maxSpeed) {
+      this.speed = spd;
+    };
+  };
+
+  drive(hours) {
+    if (this.running === true) {
+      this.distance = hours * this.speed;
+    };
+  };
+};
+
+const someCar = new Car(100);
+  someCar.turnOn();
+  someCar.drive(2);
+
+  console.log(someCar.maxSpeed);
+  console.log(someCar);
+  console.log(Car.turnOn);
+  
+  Car.getSpecs(someCar); // maxSpeed: 100, running: true, distance: 200
