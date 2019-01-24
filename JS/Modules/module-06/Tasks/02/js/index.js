8bdb71f6a3fb446185345b8f6a5b4b003f652acb
iRoy <iroy.photo@gmail.com>
2019-01-24 17:09:53 +0200

    Task 02 completed

--- JS/Modules/module-06/Tasks/02/js/index.js
+++ JS/Modules/module-06/Tasks/02/js/index.js
@@ -18,8 +18,27 @@
     - pad(str) - получает парметр str - строку и добавляет 
       ее в начало и в конец значения поля value
 */
+class StringBuilder {
+  constructor (string) {
+    this.value = string;
+  };
+  getValue() {
+    return this.value;
+  };
+  append(str) {
+    this.value = this.value + str;
+  };
+  prepend(str) {
+    this.value = str + this.value;
+  };
+  pad(str) {
+    this.value = str + this.value + str;
+  };
+};
+
 
 const builder = new StringBuilder('.');
+
 
 builder.append('^'); 
 console.log(builder.getValue()); // '.^'
