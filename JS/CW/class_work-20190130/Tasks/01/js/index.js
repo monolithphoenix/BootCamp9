924a92585b1189386f4e46ab822262c0cc118a4f
iRoy <iroy.photo@gmail.com>
2019-01-30 16:44:31 +0200

    optimizedCode

--- JS/CW/class_work-20190130/Tasks/01/js/index.js
+++ JS/CW/class_work-20190130/Tasks/01/js/index.js
@@ -4,22 +4,22 @@
 // клас fixed-nav
 
 // ===== Solution #1 =====
-    let nav = document.querySelector('.nav');
-    let header = document.querySelector('.header');
+    // let nav = document.querySelector('.nav');
+    // let header = document.querySelector('.header');
 
-    let fix = function(){
-        if (pageYOffset >= header.clientHeight) {
-            nav.classList.add('fixed-nav');
-        } else {
-            nav.classList.remove('fixed-nav');
-        };
+    // let fix = function(){
+    //     if (pageYOffset >= header.clientHeight) {
+    //         nav.classList.add('fixed-nav');
+    //     } else {
+    //         nav.classList.remove('fixed-nav');
+    //     };
 
-    // or same via arrow function
-        // pageYOffset >= header.clientHeight ? nav.classList.add('fixed-nav') : nav.classList.remove('fixed-nav');
-    }
+    // // or same via arrow function
+    //     // pageYOffset >= header.clientHeight ? nav.classList.add('fixed-nav') : nav.classList.remove('fixed-nav');
+    // }
 
-    window.addEventListener('scroll', fix)
+    // window.addEventListener('scroll', fix)
 
 
 // ===== CodeOptimizedVersion =====
-    // window.addEventListener('scroll', () => pageYOffset >= head.clientHeight ? navigation.classList.add('fixed-nav') : navigation.classList.remove('fixed-nav'))
+    window.addEventListener('scroll', () => pageYOffset >= head.clientHeight ? navigation.classList.add('fixed-nav') : navigation.classList.remove('fixed-nav'))
