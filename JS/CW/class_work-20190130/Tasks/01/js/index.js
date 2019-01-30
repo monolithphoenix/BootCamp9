d92819e083089cdfda7107c4ecec1312eef569d9
iRoy <iroy.photo@gmail.com>
2019-01-30 16:41:58 +0200

    CW progress

---
+++ JS/CW/class_work-20190130/Tasks/01/js/index.js
@@ -0,0 +1,25 @@
+// 1) При скролі сайту на велечину хедера фбо більше меню стає фіксованим
+// 2) При скролі сайту меньше ніж висота хедера меню стає на почвткову позицію
+
+// клас fixed-nav
+
+// ===== Solution #1 =====
+    let nav = document.querySelector('.nav');
+    let header = document.querySelector('.header');
+
+    let fix = function(){
+        if (pageYOffset >= header.clientHeight) {
+            nav.classList.add('fixed-nav');
+        } else {
+            nav.classList.remove('fixed-nav');
+        };
+
+    // or same via arrow function
+        // pageYOffset >= header.clientHeight ? nav.classList.add('fixed-nav') : nav.classList.remove('fixed-nav');
+    }
+
+    window.addEventListener('scroll', fix)
+
+
+// ===== CodeOptimizedVersion =====
+    // window.addEventListener('scroll', () => pageYOffset >= head.clientHeight ? navigation.classList.add('fixed-nav') : navigation.classList.remove('fixed-nav'))
