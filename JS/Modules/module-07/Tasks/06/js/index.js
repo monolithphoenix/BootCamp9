0b825c2a3995230e79d5382af1e70003874bffa8
iRoy <iroy.photo@gmail.com>
2019-02-01 17:36:27 +0200

    T06 ready

--- JS/Modules/module-07/Tasks/06/js/index.js
+++ JS/Modules/module-07/Tasks/06/js/index.js
@@ -11,5 +11,30 @@
 */
 
 function createMovieCard() {
-
+  const body = document.querySelector('body');
+  const div = document.createElement('div');
+  const img = document.createElement('img');
+  const divBody = document.createElement('div');
+  const h2 = document.createElement('h2');
+  const p = document.createElement('p');
+  const pDate = document.createElement('p');
+  const pRate = document.createElement('p');
+// Наполняем тэгами
+  div.appendChild(img).classList.add('movie__image');
+  div.appendChild(divBody).classList.add('movie__body');
+  divBody.appendChild(h2).classList.add('movie__title');
+  divBody.appendChild(p).classList.add('movie__description');
+  divBody.appendChild(pDate).classList.add('movie__date');
+  divBody.appendChild(pRate).classList.add('movie__rating');
+// Добавляем атрибуты
+  img.setAttribute('src', 'http://image.tmdb.org/t/p/w500/rPdtLWNsZmAtoZl9PK7S2wE3qiS.jpg');
+  img.setAttribute('alt', 'movie image');
+// Вставляем текст
+  h2.textContent = 'The Godfather';
+  p.textContent = 'The Spanning the years 1945 to 1955, a chronicle of the fictional Italian-American Corleone crime family. When organized crime family patriarch, Vito Corleone barely survives an attempt on his life, his youngest son, Michael steps in to take care of the would-be killers, launching a campaign of bloody revenge.';
+  pDate.textContent = 'Released: 1972-03-14';
+  pRate.textContent = 'Rating: 8.6';
+// Выводим карточку в <body>
+  return body.appendChild(div).classList.add('movie');
 }
+createMovieCard()
