476ebdbc8908f3ab41b7ec2d971466c5ac636d43
iRoy <iroy.photo@gmail.com>
2019-02-04 10:58:04 +0200

    тщетные попытки решить

--- JS/Modules/module-08/Tasks/02/js/index.js
+++ JS/Modules/module-08/Tasks/02/js/index.js
@@ -3,3 +3,19 @@
   Даны 2 инпута, абзац и кнопка. По нажатию на кнопку 
   получите числа которые бьудут введены в инпуты и запишите их сумму в span.result.
 */
+const add = document.querySelector('button[data-action="add"]');
+const result = document.querySelector('.result');
+const expression = document.querySelector('.expression');
+let a;
+let b;
+
+expression.addEventListener(, getArg);
+add.addEventListener('click', getSum);
+
+function getSum() {
+
+  result.textContent
+}
+console.log(add);
+console.log(result);
+console.log(expression.firstElementChild);
