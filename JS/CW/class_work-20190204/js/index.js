f9a12a372a107b3c02a632a69c9f146bc45a7b7e
iRoy <iroy.photo@gmail.com>
2019-02-04 11:46:02 +0200

    ADD CW 0204

---
+++ JS/CW/class_work-20190204/js/index.js
@@ -0,0 +1,9 @@
+const URL = 'https://pixabay.com/api/?key=5018958-ed49ccd90878e6614abdf24a6&image_type=photo&category=backgrounds&per_page=12';
+
+fetch(URL)
+
+// CRUD
+// 1) GET - READ
+// 2) POST - CREATE
+// 3) DELETE - DELETE
+// 4) PUT - UPDATE/UPGRADE
