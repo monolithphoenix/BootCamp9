7e94d2547d8e56399f0a5f58077dc5bc643a3a95
iRoy <iroy.photo@gmail.com>
2019-02-04 09:46:32 +0200

    M08 T01 Ready

--- JS/Modules/module-08/Tasks/01/js/index.js
+++ JS/Modules/module-08/Tasks/01/js/index.js
@@ -1,43 +1,13 @@
 // Module 07
 /* Task 01
-  Есть список категорий с классом categories (на вкладке HTML).
+  Напишите скрипт который реализует следующий функционал.
   
-  Напишите код, который для каждого элемента li (первая вложенность) 
-  в списке categories выведет в консоль:
-  - Текст непосредственно в нём (название категории)
-  - Количество всех вложенных в него элементов li
-  
-  К примеру:
-    Категория: Животные
-    Количество вложенных li: 4
+  Есть кнопка с классом button, текст которой отображает 
+  кол-во раз которое по ней кликнули, обновляется при каждом клике.
 */
 
-let catArr = Array.from(document.querySelector('.categories').children);
-
-for (const el of catArr) {
-    let catElCont = el.children[0].children.length;
-    let catElName = el.firstChild.wholeText.trim();
-    console.log(`Категория: ${catElName}
-Количество вложенных li: ${catElCont}`);
+const btn = document.querySelector('button');
+btn.addEventListener('click', upCount);
+function upCount() {
+  ++btn.textContent;
 }
-
-
-// console.log(cat.firstElementChild.lastElementChild.childElementCount);
-// console.log(cat.children);
-// console.log(cat.children[0]);
-// console.log(cat.children[0].firstChild.textContent);
-// console.log(cat.children[0].firstChild.textContent.trim());
-
-// console.log(cat.children[0].children[0].textContent);
-// console.log(cat.children[0].childNodes);
-// console.log(cat.children[0].childNodes[0]);
-
-
-// console.log(cat.children[0].firstChild.textContent.trim());
-// console.log(cat.children[1].firstChild.textContent.trim());
-// console.log(cat.children[2].firstChild.textContent.trim());
-
-// console.log('Колличество вложенных элементов:');
-// console.log(cat.children[0].children[0].children.length);
-// console.log(cat.children[1].children[0].children.length);
-// console.log(cat.children[2].children[0].children.length);
