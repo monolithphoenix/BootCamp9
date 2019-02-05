90e8201152efb52408023bccceeb1c35d4b0b0ea
iRoy <iroy.photo@gmail.com>
2019-02-05 10:53:35 +0200

    T06 ready

--- JS/Modules/module-08/Tasks/06/js/index.js
+++ JS/Modules/module-08/Tasks/06/js/index.js
@@ -1,6 +1,15 @@
-// Module 07
+// Module 08
 /* Task 06
   Дан ul, а внутри него произвольное количество li с текстом и кнопкой. 
   Сделайте так, чтобы по нажатию на кнопку, удалялся тот li в котором
   она находится. Обязательно используйте делегирование событий.
 */
+
+const LIST = document.querySelector('.list');
+LIST.addEventListener('click', deleteTargetLi);
+function deleteTargetLi(event) {
+  if (event.target.nodeName !== 'BUTTON') {return}
+  // console.log(event.target);
+  // console.log(event.target.parentNode);
+  event.target.parentNode.remove()
+}
