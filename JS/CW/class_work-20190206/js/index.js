c885d802059861a4a43dda47439cefc7d68baa5c
iRoy <iroy.photo@gmail.com>
2019-02-06 17:43:40 +0200

    clear JS

--- JS/CW/class_work-20190206/js/index.js
+++ JS/CW/class_work-20190206/js/index.js
@@ -2,18 +2,13 @@
 // resolve - функция, которая выполняется при удачном выполнении
 // reject - функция, которая выполняется при ошибке
 // const pro = new Promise (function(resolve, reject) => {}) 
+
 const trackList = document.querySelector('.tracks')
 const ul = document.createElement('ul');
 const lastFMGeoTracks = 'http://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks&country=ukraine&api_key=412e51e107155c7ffabd155a02371cbd&limit=25&format=json';
-// let ObjGeoTracks = {};
 
 trackList.appendChild(ul);
 
-// trackList.addEventListener('click', changeImgSize);
-// function changeImgSize(e) { 
-//     // console.log(e.target.nodeName);
-//     if (e.target.nodeName !== 'IMG') {return}
-// }
 
 getTracks(lastFMGeoTracks);
 function getTracks(link) {
@@ -23,27 +18,7 @@ function getTracks(link) {
         .catch(err => console.log(err))
 }
 
-// console.log(ObjGeoTracks);
-// function getObj(obj) {
-//     console.log(obj);
-//     ObjGeoTracks = obj;
-//     console.log(obj.track.length);
-//     let cont = '';
-//         for (let i = 0; i < obj.track.length; i++) {
-//             cont += `<ul><li>
-//             <img src=${obj.track[i].image[0]['#text']} alt="">
-//             <span class='artistName'>${obj.track[i].artist.name}</span>
-//             <span class='trackName'>${obj.track[i].name}</span>
-//             </li></ul>`
-//         }
-//         trackList.innerHTML = cont;
-// }
-
-function getObj(obj) {
-    // console.log(obj);
-    // ObjGeoTracks = obj;
-    // console.log(obj.track.length);
-   
+function getObj(obj) {  
     for (let i = 0; i < obj.track.length; i++) {        
         ul.innerHTML += `<li>
         <img src=${obj.track[i].image[0]['#text']} alt="">
@@ -52,10 +27,3 @@ function getObj(obj) {
         </li>`
     }
 }
-
-// console.log(ObjGeoTracks);
-
-// createTrackList
-//tracks.track[""0""].image[""0""][""#text""]
-//tracks.track['0'].image['0']['#text']
-// track[""0""].artist.name
