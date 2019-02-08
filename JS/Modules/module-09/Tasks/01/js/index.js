02f6663a0344c2305be742ffdd0d6adccc8a07c2
iRoy <iroy.photo@gmail.com>
2019-02-08 17:35:16 +0200

    T01 ready

--- JS/Modules/module-09/Tasks/01/js/index.js
+++ JS/Modules/module-09/Tasks/01/js/index.js
@@ -12,3 +12,29 @@
 */
 
 const colors = ['#FFFFFF', '#F44336', '#2196F3', '#4CAF50', '#FF9800', '#009688', '#795548'];
+
+const START = document.querySelector('.js-start');
+const STOP = document.querySelector('.js-stop');
+let count = 0;
+START.addEventListener('click', changeColor);
+STOP.addEventListener("click", stopChangeColor);
+
+// document.body.style = `background-color: #F44336;`;
+// console.log(document.body);
+
+function changeColor() {
+  // let random = Math.floor(Math.random()*colors.length);
+  // console.log(random);
+  // console.log(colors[random]);
+  if(count > 0){return};
+    ++count;
+    timerID = setInterval(() => {
+      let random = Math.floor(Math.random()*colors.length);
+      document.body.style = `background-color: ${colors[random]};`
+    }, 1000);  
+};
+
+function stopChangeColor() {
+  clearInterval(timerID);
+  --count;
+};
