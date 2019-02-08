df919b27cff565c270bbdae641e3fddbcfeb0b85
iRoy <iroy.photo@gmail.com>
2019-02-08 18:07:08 +0200

    add T03

---
+++ JS/Modules/module-09/Tasks/03/js/index.js
@@ -0,0 +1,60 @@
+// Module 09
+/* Task 03 
+  Напишите скрипт, реализующий базовый функционал
+  таймера, запуск отсчета времени и сброс счетчика
+  в исходное состояние.
+  
+  Используйте возможности объекта Date для работы со временем.
+  
+  Создайте функцию startTimer, которая будет запускать
+  отсчет времени с момента ее нажатия, она вызывается 
+  при клике на кнопку с классом js-timer-start.
+  
+  Создайте функцию stopTimer, которая будет останавливать
+  счетчик, она вызывается при клике на кнопку с классом js-timer-stop.
+  
+  Используйте вспомогательную функцию updateClockface 
+  которая обновляет значение счетчика в интерфейсе. 
+  Для составления строки времени в формате xx:xx.x, 
+  исользуйте функцию getFormattedTime из задания номер 3.
+  
+  Подсказка: так как нам интересны исключительно сотни миллисекунд,
+      нет смысла выполнять пересчет времени чаще чем каждые 100мс.
+*/
+
+const clockface = document.querySelector(".js-clockface");
+const startBtn = document.querySelector(".js-timer-start");
+const stopBtn = document.querySelector(".js-timer-stop");
+
+const timer = {
+  startTime: null,
+  deltaTime: null,
+  id: null
+};
+
+/*
+* Вспомогательные функции
+*/
+
+/*
+* Обновляет поле счетчика новым значением при вызове
+* аргумент time это кол-во миллисекунд
+*/
+function updateClockface(elem, time) {
+  // Используйте функцию getFormattedTime из задания #1
+  // elem.textContent = getFormattedTime(time);
+}
+
+/*
+* Подсветка активной кнопки
+*/
+function setActiveBtn(target) {
+  if(target.classList.contains('active')) {
+    return;
+  }
+  
+  startBtn.classList.remove('active');
+  stopBtn.classList.remove('active');
+  
+  target.classList.add('active');
+}
