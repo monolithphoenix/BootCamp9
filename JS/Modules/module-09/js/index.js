7327fad9702dc897473bc1bfee73b74a82553f8b
iRoy <iroy.photo@gmail.com>
2019-02-12 10:57:49 +0200

    +++

--- JS/Modules/module-09/js/index.js
+++ JS/Modules/module-09/js/index.js
@@ -44,6 +44,57 @@
   - Функционал кнопки button.js-take-lap при клике - сохранение текущего времени секундомера 
     в массив и добавление в ul.js-laps нового li с сохраненным временем в формате xx:xx.x
 */
+const clockface = document.querySelector(".js-time");
+const startBtn = document.querySelector(".js-start");
+const resetBtn = document.querySelector(".js-reset");
+startBtn.addEventListener('click', startTimer);
+resetBtn.addEventListener('click', stopTimer);
+
+function startTimer() {
+  const STARTTIME = Date.now();
+  
+  ID = setInterval(() => {
+    let newTime = Date.now();
+    let diferense = newTime-STARTTIME;
+    console.log(diferense);
+    
+    updateClockface(clockface, diferense);
+  }, 100);
+  console.log(startBtn.value);
+};
+function stopTimer() {
+  clearInterval(ID);
+}
+const timer = {
+  startTime: null,
+  deltaTime: null,
+  id: null
+};
+
+function updateClockface(elem, time) {
+  elem.textContent = getFormattedTime(time);
+}
+
+/*
+* Подсветка активной кнопки
+*/
+function setActiveBtn(target) {
+  if(target.classList.contains('active')) {return;};
+  
+  startBtn.classList.remove('active');
+  stopBtn.classList.remove('active');
+  
+  target.classList.add('active');
+}
+function getFormattedTime(time) {
+  let date = new Date(time);
+  let min = date.getMinutes();
+  let sec = date.getSeconds();
+  let mil = Math.floor(date.getMilliseconds()/100);
+  if(min<=9)(min='0'+min.toString());
+  if(sec<=9)(sec='0'+sec.toString());
+  return `${min}:${sec}.${mil}`;
+}
 
 /*
   ⚠️ ЗАДАНИЕ ПОВЫШЕННОЙ СЛОЖНОСТИ - ВЫПОЛНЯТЬ ПО ЖЕЛАНИЮ
