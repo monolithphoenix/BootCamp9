dc87b874958656fe411bb27fc65dddd9d44e18cc
iRoy <iroy.photo@gmail.com>
2019-02-13 19:00:13 +0200

    T04 ready

--- JS/Modules/module-10/Tasks/04/js/index.js
+++ JS/Modules/module-10/Tasks/04/js/index.js
@@ -12,13 +12,36 @@
   Если пользователя с таким идентификатором в базе данных нет,
   в элемент .result вывести строку "Ошибка! Пользователя с таким id не существует"
 */
-
+const URL = 'https://jsonplaceholder.typicode.com/users/'
 const input = document.querySelector("input");
 const form = document.querySelector(".search-form");
 const result = document.querySelector(".result");
 
 form.addEventListener("submit", getUserById);
 
+// function getUserById(evt) {
+//   evt.preventDefault();
+//   console.log(input.value);
+//   fetch(URL+input.value)
+//     .then(res => res.json())
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err));
+// }
+
 function getUserById(evt) {
-  // ...
+  evt.preventDefault();
+  // console.log(input.value);
+  fetch(URL+input.value)
+    .then(res => res.ok ? res.json() : result.innerHTML = '<p style="text-align:center">Ошибка! Пользователя с таким id не существует</p>')
+    .then(data => result.innerHTML = `
+    <table class="users-table">
+      <tr>
+        <td>${data.name}</td>
+        <td>${data.email}</td>
+        <td>${data.address.city}</td>
+        <td>${data.website}</td>
+        <td>${data.company.name}</td>
+      </tr>
+    </table>`)
+    .catch(err => console.log(err));
 }
