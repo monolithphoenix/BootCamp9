3fa6d68dd2842e73ddec34eeb9979f01d5d98708
iRoy <iroy.photo@gmail.com>
2019-02-13 07:18:34 +0200

    M10 T01 ready

--- JS/Modules/module-10/Tasks/01/js/index.js
+++ JS/Modules/module-10/Tasks/01/js/index.js
@@ -32,6 +32,24 @@ form.addEventListener("submit", fetchCountryData);
 /*
   @param {FormEvent} evt
 */
+
 function fetchCountryData(evt) {
-  // ...
+  evt.preventDefault();
+  const askAPI = API_URL+input.value;
+  console.log(askAPI);
+  fetch(askAPI)
+    .then(res => res.json())
+    .then(data => getData(data))
+    .catch(err => console.log(err));
+}
+
+function getData(data) {
+  const resultData = data[0];
+  console.log(resultData);
+  result.innerHTML = `
+    <ul style='list-style:none'>
+      <li><p>Country name: ${resultData.name}</p></li>
+      <li><p>Capital: ${resultData.capital}</p></li>
+      <li><p>Main currency:  ${resultData.currencies[0].name}</p></li>
+      <li><p>Flag:</p><p><img src=${resultData.flag} alt='flag' width='320'></p></li>`    
 }
