8748de06ea0cb8b74c60a879409f1a026b4f8b1e
iRoy <iroy.photo@gmail.com>
2019-02-13 07:23:14 +0200

    add T02

---
+++ JS/Modules/module-10/Tasks/02/js/index.js
@@ -0,0 +1,34 @@
+// Module 10
+/* Task 02
+  Написать функцию fetchUserData, которая использует
+  API_URL + текущее значение input для составления запроса.
+  
+  Формат полного url таков:
+    https://api.github.com/users/имя-пользователя
+    
+  Документация по Git API:
+    https://developer.github.com/v3/
+    
+  С помощью fetch сделать запрос по составленому адресу. 
+  Обязательно обработать вариант с ошибкой запроса используя catch. 
+  
+  Результат запроса вывести в поле result в формате:
+    Avatar: аватартка 
+    Username: логин
+    Bio: описание профиля
+    Public repos: кол-во открытых репозиториев
+  
+  Все необходимые данные есть в ответе от API.
+*/
+
+const input = document.querySelector("input");
+const form = document.querySelector(".search-form");
+const result = document.querySelector(".result");
+const API_URL = "";
+
+form.addEventListener("submit", fetchUserData);
+
+/*
+  @param {FormEvent} evt
+*/
+function fetchUserData(evt) {}
